1d0214f47609b570d3a2d63de69c83cf52f21d2c
Luke A. Smith <112637579+lasmit17@users.noreply.github.com>
2023-05-07 23:30:07 +0200

    Add files via upload

--- radix.py
+++ radix.py
@@ -33,12 +33,55 @@ def countingSort(array, place):
 
 
 # Main function to implement radix sort
-def radixSort(array):
-    # Get maximum element
-    max_element = max(array)
+#def radixSort(array):
+ #   # Get maximum element
+ #   max_element = max(array)
 
     # Apply counting sort to sort elements based on place value.
-    place = 1
-    while max_element // place > 0:
-        countingSort(array, place)
-        place *= 10
+  #  place = 1
+   # while max_element // place > 0:
+    #    countingSort(array, place)
+     #   place *= 10
+        
+
+def radix_sort(arr):
+    # Find the maximum number to know the number of digits
+    max_num = max(arr)
+    # Do radix sort using counting sort as the sorting algorithm
+    exp = 1
+    while max_num // exp > 0:
+        arr = counting_sort(arr, exp)
+        exp *= 10
+    return arr
+
+def counting_sort(arr, exp):
+    n = len(arr)
+    # Create a count array to store count of individual digits
+    count = [0] * 10
+    # Store count of occurrences in count[]
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
+    # Change count[i] so that it now contains actual position of this digit in output[]
+    for i in range(1, 10):
+        count[i] += count[i-1]
+    # Build the output array
+    output = [0] * n
+    for i in range(n-1, -1, -1):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+    # Copy the output array to arr[]
+    for i in range(n):
+        arr[i] = output[i]
+    return arr
+
+
+        
+array = [170, 45, 75, 90, 802, 24, 2, 66]
+print("Original array:", array)
+sorted_array = radix_sort(array)
+print("Sorted array:", sorted_array)
+
+
+
